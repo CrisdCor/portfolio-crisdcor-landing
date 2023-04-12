2bc57334d68d3f6c1fa2993bd35d870fb8a793fb
Usuario <cristiandavid.corrales@gmail.com>
2023-04-12 13:42:40 -0500

    Se crea funcionalidad en el botón menú y actualizan routes de CSS

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
 const buttonMenu = document.querySelector('.header--button-menu');
+const textButtonMenu = document.querySelector('.button-menu--text');
 const menu = document.querySelector('.menu');
-const textButtonMenu = document.querySelector('.header--button-menu p');
+
 
 buttonMenu.addEventListener('click', openCloseMenu);
 
 function openCloseMenu() {
+    if (textButtonMenu.textContent==="MENÚ"){
+        textButtonMenu.textContent="CERRAR";
+
+
+    }else{
+        textButtonMenu.textContent="MENÚ";
+    }
     menu.classList.toggle('open-menu');
     menu.classList.toggle('close-menu');
+    buttonMenu.classList.toggle('header--button-menu-close')
+
 
 }
+
