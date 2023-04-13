bd0c0cebd6c90cd2fe7eaf7fd3f23eb3b15ac9b5
Usuario <cristiandavid.corrales@gmail.com>
2023-04-12 21:54:23 -0500

    Se realiza activación de navegadores del menú

--- src/main.js
+++ src/main.js
@@ -1,22 +1,49 @@
 const buttonMenu = document.querySelector('.header--button-menu');
 const textButtonMenu = document.querySelector('.button-menu--text');
 const menu = document.querySelector('.menu');
+const navHome = document.querySelector('.navbar--home');
+const navAbout = document.querySelector('.navbar--about');
+const navProject = document.querySelector('.navbar--projects');
+const navContact = document.querySelector('.navbar--contact');
 
 
-buttonMenu.addEventListener('click', openCloseMenu);
-
-function openCloseMenu() {
+function upGradeButtonMenuText(){
     if (textButtonMenu.textContent==="MENÚ"){
         textButtonMenu.textContent="CERRAR";
-
-
     }else{
         textButtonMenu.textContent="MENÚ";
     }
+}
+
+function openCloseMenu(){
     menu.classList.toggle('open-menu');
     menu.classList.toggle('close-menu');
     buttonMenu.classList.toggle('header--button-menu-close')
-
-
 }
+
+
+buttonMenu.addEventListener('click', function(){
+    upGradeButtonMenuText();
+    openCloseMenu();
+});
+
+navHome.addEventListener('click', function(){
+    upGradeButtonMenuText();
+    openCloseMenu();
+});
+
+navAbout.addEventListener('click', function(){
+    upGradeButtonMenuText();
+    openCloseMenu();
+});
+
+navProject.addEventListener('click', function(){
+    upGradeButtonMenuText();
+    openCloseMenu();
+});
+
+navContact.addEventListener('click', function(){
+    upGradeButtonMenuText();
+    openCloseMenu();
+});
 
